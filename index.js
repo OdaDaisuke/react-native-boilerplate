b26df02ee3175e1d5a0b2281ef583e9cb79748bb
OdaDaisuke <hinopon25@gmail.com>
2018-11-16 23:00:03 +0900

    feat:introduce Expo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/** @format */
+import React from 'react'
+import Expo from 'expo'
+import App from './App'
+import Root from './src'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export default Expo.registerRootComponent(() => <App><Root /></App>)
