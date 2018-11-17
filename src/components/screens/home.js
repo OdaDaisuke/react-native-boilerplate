3fcac6de54a4aaec08bc64e36d0dbc673eea9fa1
OdaDaisuke <hinopon25@gmail.com>
2018-11-17 13:54:30 +0900

    feat:introduce flow typed

--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -1,14 +1,17 @@
+// @flow
+
 import React from 'react'
 import { View, Text } from 'react-native'
 import Button from '../parts/Button'
 
-export default class Home extends React.Component {
-    render() {
-        return (
-            <View>
-                <Button>Press me</Button>
-                <Text>home screen</Text>
-            </View>
-        )
-    }
+type PropsType = {
+}
+
+export default (props: PropsType) =>{
+    return (
+        <View>
+            <Button>Press me</Button>
+            <Text>home screen</Text>
+        </View>
+    )
 }
