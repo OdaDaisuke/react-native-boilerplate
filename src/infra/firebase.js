4aa8403c53f6c2b325072f74d4e5e7952afd90dd
OdaDaisuke <hinopon25@gmail.com>
2018-11-17 14:37:17 +0900

    feat:introduce env variables system

--- src/infra/firebase.js
+++ src/infra/firebase.js
@@ -1,12 +1,18 @@
 import * as firebase from 'firebase'
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DB_URL,
+    FIREBASE_STORAGE_BUCKET
+} from 'react-native-dotenv'
 
 // Initialize Firebase
 const init = () => {
     const firebaseConfig = {
-        apiKey: "<YOUR-API-KEY>",
-        authDomain: "<YOUR-AUTH-DOMAIN>",
-        databaseURL: "<YOUR-DATABASE-URL>",
-        storageBucket: "<YOUR-STORAGE-BUCKET>",
+        apiKey: FIREBASE_API_KEY,
+        authDomain: FIREBASE_AUTH_DOMAIN,
+        databaseURL: FIREBASE_DB_URL,
+        storageBucket: FIREBASE_STORAGE_BUCKET,
     }
 
     firebase.initializeApp(firebaseConfig)
